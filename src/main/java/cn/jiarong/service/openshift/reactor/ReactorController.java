de30693a96d4a877c92e2cb45026c2ab92e599ae
老莫_05180 <mojiezhong05180@126.com>
2022-01-18 10:15:49 +0800

    ok

--- src/main/java/cn/jiarong/service/openshift/reactor/ReactorController.java
+++ src/main/java/cn/jiarong/service/openshift/reactor/ReactorController.java
@@ -1,5 +1,6 @@
 package cn.jiarong.service.openshift.reactor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,13 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class ReactorController {
 
     @GetMapping("/helloWorld/{id}")
     public Mono<String> helloWorld(@PathVariable("id") Long id){  //1
-        return Mono.just(id + "hello world_" + System.currentTimeMillis()); //2
+        String str = id + " hello world_" + System.currentTimeMillis();
+        log.info(str);
+        return Mono.just(str); //2
     }
 }
